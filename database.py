032012231f64a52629b230cdb3a3a685429f1aad
Carlo Ledesma <carlo.miguel.ledesma@gmail.com>
2014-09-16 12:14:02 +0800

    Added creation of tables

---
+++ database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Watcher(Base):
+	__tablename__ = 'watcher'
+	id = Column(Integer, primary_key=True)
+	email = Column(String, nullable=False)
+
+class Node(Base):
+	__tablename__= 'node'
+	id = Column(Integer, primary_key=True)
+	url = Column(String, nullable=False)
+
+def init():
+	db = create_engine('sqlite:///websight.db')
+	print db
+	Base.metadata.create_all(db)
