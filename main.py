779d9cdd1078fb2dde834d7a2006430feb9b0921
Carlo Ledesma <carlo.miguel.ledesma@gmail.com>
2014-09-17 14:10:27 +0800

    Loop every minute + added requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from websight_service import WebsightService
+from apscheduler.schedulers.background import BackgroundScheduler
+import logging 
 
+logging.basicConfig()
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,5 +11,16 @@ def hello():
     return "Hello World!"
 
 if __name__== "__main__":
+    print "starting websight"
+    srv = WebsightService()
+    scheduler = BackgroundScheduler()
+
+    @scheduler.scheduled_job('interval', minutes=1)
+    def loop_job():
+        srv.loop_through_nodes()
+
+    print "begin scheduler"
+    scheduler.start()
+    print "begin app server"
     app.run()
 
